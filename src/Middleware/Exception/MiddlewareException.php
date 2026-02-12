732c4ae7738575b2397cde98847cac12fdf1738b
vgalitsky <concept.galitsky@gmail.com>
2026-02-12 09:02:02 -0500

    decouple middleware to separate package

--- src/Middleware/Exception/MiddlewareException.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Juzdy\Http\Middleware;
-
-use Exception;
-
-class MiddlewareException extends Exception implements MiddlewareExceptionInterface
-{
-}
--- src/Middleware/Exception/MiddlewareExceptionInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Juzdy\Http\Middleware;
-
-interface MiddlewareExceptionInterface extends \Throwable
-{
-    // Marker interface for exceptions thrown by middleware components
-}
